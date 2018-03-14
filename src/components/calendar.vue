9ad0b16862646244414e2874e1fd29a582fd6cee
熊杰 <285516193@qq.com>
2018-03-14 16:25:09 +0800

    允许上层自定义类，但是不支持异步

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="calendar_text">
-    <text>
-      123
-    </text>
+  <div>
+    <div class="calendar_day_row" v-for="(arr, key) in days" :key="key">
+      <text class="calendar_day_col" :class="itemCls(year, month, day)" v-for="(day) in arr" :key="day">
+        {{day}}
+      </text>
+    </div>
   </div>
-  
 </template>
 <script>
+import { getMaxDay, chunk } from "./c";
 export default {
-    
-}
+  props: {
+    year: Number,
+    month: Number,
+    itemCls: {
+      type: Function,
+      default: () => ''
+    }
+  },
+  computed: {
+    days() {
+      const maxDay = getMaxDay(this.year, this.month)
+      const allDays = Array.from({length: maxDay}).map((_, k) => k + 1)
+      return chunk(allDays, 7)
+    }
+  }
+};
 </script>
 
 <style scoped>
-.calendar_text {
-  color: red;
+.calendar_day_row {
+  display: flex;
 }
 </style>
