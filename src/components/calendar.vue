ac7be0e13feecd4e8380ee481d013386b3b7affd
徐媛 <xuy04@mingyuanyun.com>
2018-03-14 18:13:09 +0800

    日历

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -4,11 +4,20 @@
           <text class="calendar_header_year">2018</text>
           <text class="calendar_header_month">/1月</text>
       </div>
-    <div class="calendar_day_row" v-for="(arr, key) in days" :key="key">
-      <text class="calendar_day_col" :style="itemStyle(year, month, day)" v-for="(day) in arr" :key="day">
-        {{day}}
-      </text>
-    </div>
+      <div class="calender_header_week">
+          <text class="calendar_week">一</text>
+          <text class="calendar_week">二</text>
+          <text class="calendar_week">三</text>
+          <text class="calendar_week">四</text>
+          <text class="calendar_week">五</text>
+          <text class="calendar_week">六</text>
+          <text class="calendar_week">日</text>
+      </div>
+      <div class="calendar_day_row" v-for="(arr, key) in days" :key="key">
+          <text class="calendar_day_col" :style="itemStyle(year, month, day)" v-for="(day) in arr" :key="day">
+              {{day}}
+          </text>
+      </div>
   </div>
 </template>
 <script>
@@ -35,29 +44,43 @@ export default {
 <style scoped>
     .calendar_header{
         flex-direction: row;
-        align-items: baseline;
+        -webkit-flex-direction: row;
+        align-items: flex-end;
+        -webkit-align-items: flex-end;
+        padding: 30px 60px;
         background-color: #1dbe8c;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
     }
     .calendar_header_year,
     .calendar_header_month{
         color: #fff;
     }
     .calendar_header_year{
-        font-size: 30px;
+        font-size: 40px;
         font-weight: bold;
     }
     .calendar_header_month{
-        font-size: 12px;
+        font-size: 24px;
+    }
+    .calender_header_week{
+        flex-direction: row;
+        -webkit-flex-direction: row;
+    }
+    .calendar_week{
+        flex: 1;
+        -webkit-flex: 1;
+        text-align: center;
+        line-height: 60px;
     }
   .calendar_day_row{
     flex-direction: row;
-    align-items: center;
+      -webkit-flex-direction: row;
   }
-  .calendar_day_col{
-    width: 14.28%;
-    height: 80px;
-    line-height: 80px;
-    white-space: nowrap;
-    text-align: center;
-  }
+    .calendar_day_col{
+        flex: 1;
+        -webkit-flex: 1;
+        text-align: center;
+        line-height: 20px;
+    }
 </style>
